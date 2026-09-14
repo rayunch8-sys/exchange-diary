21126d148177aa470a9105447ab585bad59a721c
rayunch8-sys <rayun.ch8@gmail.com>
2026-09-14 22:56:28 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// GitHub Pages 프로젝트 사이트(https://<아이디>.github.io/<레포이름>/)로 배포한다면
-// base를 반드시 '/레포이름/' 형태로 바꿔주세요.
-// 예: 레포 이름이 'our-diary'라면 base: '/our-diary/'
-// (User/Organization Pages, 즉 <아이디>.github.io 레포라면 base: '/' 유지)
+// https://vitejs.dev/config/
 export default defineConfig({
   base: '/exchange-diary/',
   plugins: [react()],
-  base: '/our-diary/',
 })
